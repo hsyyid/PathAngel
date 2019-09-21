2125fe5c2c438aaeb972db71c3af05cf6a5f6d20
Hassan Syyid <hassansyyid@gmail.com>
2019-09-21 12:11:19 -0400

    APIed the shit out of it

--- main.py
+++ main.py
@@ -1,26 +1,81 @@
-from flask import make_response, jsonify
+from flask import Flask, request, make_response, jsonify
 
 import googlemaps
+import polyline
+import pandas as pd
 from datetime import datetime
+
+from selenium import webdriver
+
+# Start Selenium
+driver = webdriver.Chrome()
+driver.implicitly_wait(1)
 
 gmaps = googlemaps.Client(key='***REMOVED***')
 
+app = Flask(__name__)
 
-def get_path(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
 
-    # GET DIRECTIONS FROM GOOGLE CLOUD
-    request_json = request.get_json()
-    location = request_json['location']
-    destination = request_json['destination']
+def to_state_coords(coords):
+    # Unwrap Tuple
+    lat, long = coords
 
+    # Load the site
+    driver.get("http://www.earthpoint.us/StatePlane.aspx")
+
+    # Input output format
+    driver.find_element_by_id("ContentPlaceHolder1_Zone2").send_keys("3104")
+
+    # Input latitude
+    driver.find_element_by_id("ContentPlaceHolder1_Latitude").send_keys(str(lat))
+
+    # Input longitude
+    driver.find_element_by_id("ContentPlaceHolder1_Longitude").send_keys(str(long))
+
+    # Press the button
+    driver.find_element_by_id("ContentPlaceHolder1_btnLatLonCalc_Button1").click()
+
+    # Get output
+    content = driver.find_element_by_css_selector(
+        'div > table > tbody > tr > td > table > tbody > tr:nth-child(9) > td:nth-child(2)')
+    result = content.text
+
+    x = result[5:result.index('ftUSE')]
+    y = result[result.index('ftUSE') + 6:result.index('ftUSN')]
+
+    return float(x), float(y)
+
+
+def weight_path(path, radius=5):
+    # Convert to coords
+    path = [to_state_coords(coords) for coords in path]
+    print("CONVERTED PATH")
+    print(path)
+
+    # Read data
+    df = pd.read_csv('felony_data.csv')
+    total_crime = 0
+
+    for coords in path:
+        x, y = coords
+        res = df[
+            df['X_COORD_CD'].between(x - radius, x + radius) &
+            df['Y_COORD_CD'].between(y - radius, y + radius)
+            ]
+        crime = len(res)
+        total_crime += crime
+
+    avg_crime = total_crime / len(path)
+    print(f'{total_crime} -> {avg_crime}')
+    return avg_crime
+
+
+@app.route('/find_path')
+def find_safest_path():
+    location = request.args.get('location')
+    destination = request.args.get('destination')
+
+    # Get directions from Google Maps API
     now = datetime.now()
     directions_result = gmaps.directions(location,
                                          destination,
@@ -28,23 +83,23 @@ def get_path(request):
                                          departure_time=now,
                                          alternatives=True)
 
-    # TODO: Decode directions result
+    print(f"Routes returned by Google: {len(directions_result)}")
 
-    # WEIGHT DIRECTIONS USING CRIME DATA
-    # RETURN BEST PATH
+    # Get the encoded polyline paths
+    encoded_paths = [route.get('overview_polyline').get('points') for route in directions_result]
+    print(encoded_paths)
 
-    return make_response(jsonify(message='Done'), 200)
+    # Decode the polyline paths
+    decoded_paths = [polyline.decode(path) for path in encoded_paths]
+    print("LAT/LONG PATH")
+    print(decoded_paths)
 
+    # Get the weights based on crime data
+    weights = [weight_path(path) for path in decoded_paths]
+    print(weights)
 
-if __name__ == "__main__":
-    from flask import Flask, request
+    # Pick the best path (lowest weight)
+    best_path = weights.index(min(weights))
+    print(best_path)
 
-    app = Flask(__name__)
-
-
-    @app.route('/')
-    def index():
-        return get_path(request)
-
-
-    app.run('127.0.0.1', 8000, debug=True)
+    return make_response(jsonify(best_path=encoded_paths[best_path]), 200)
